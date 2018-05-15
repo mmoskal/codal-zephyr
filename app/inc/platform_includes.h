81f2216e50a1a0681a10d74015c3f3ce6124dc14
Michal Moskal <michal@moskal.me>
2018-05-15 10:24:28 -0700

    Starting on codal

---
+++ app/inc/platform_includes.h
@@ -0,0 +1,12 @@
+#ifndef PLATFORM_INCLUDES
+#define PLATFORM_INCLUDES
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <math.h>
+
+#define PROCESSOR_WORD_TYPE uint32_t
+
+#endif
