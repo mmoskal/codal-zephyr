86a397e97ed9d74da60840bf0472435bb74baf4c
Michal Moskal <mimoskal@microsoft.com>
2018-05-15 17:12:49 -0700

    It builds

--- app/inc/platform_includes.h
+++ app/inc/platform_includes.h
@@ -7,6 +7,10 @@
 #include <stdarg.h>
 #include <math.h>
 
+#include <linker/linker-defs.h>
+
 #define PROCESSOR_WORD_TYPE uint32_t
+#define DEVICE_STACK_BASE __kernel_ram_end
+#define DEVICE_STACK_SIZE 0
 
 #endif
